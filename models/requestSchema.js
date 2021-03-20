dae3b8b7ecbfd08a875a875c0326291c1b5d1f8f
Anmol Sahu <anmolsahu2k@gmail.com>
2021-03-20 20:01:49 +0530

    [ADD] Add requestSchema

---
+++ models/requestSchema.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const requestSchema = new mongoose.Schema({
+    typeOfRequest: String,
+    handler: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        username: String,
+    },
+    sourceLocation: {
+        lat: String,
+        long: String
+    },
+    message: String,
+    generatedAt: { type: Date, default: Date.now },
+    currentStatus: { type: String, default: "Active" },
+    requestedUsers: [String],
+    acceptedUsers: [String]
+
+})
+
+module.exports = mongoose.model("Request", requestSchema)
